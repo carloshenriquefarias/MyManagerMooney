bb5cec184ef6ab423575d1e671813cad1396e4e4
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-09-22 17:52:22 -0400

    refatorando

--- src/components/Sidebar/novasidebar.tsx
+++ src/components/Sidebar/novasidebar.tsx
@@ -8,23 +8,7 @@ export function SidebarNav(){
     return(
         <Stack spacing="12" align="flex-start">
 
-            <NavSection title='LANÇAMENTOS'>
-                <NavLink icon={RiContactsLine} href="/lancamentos/newtransaction">Nova Transação</NavLink>
-                <NavLink icon={RiContactsLine} href="/lancamentos/cadastros">Cadastros</NavLink>  
-                <NavLink icon={RiContactsLine} href="/lancamentos">Lançamentos</NavLink>                                              
-            </NavSection>
-
-            <NavSection title='RELATÓRIOS'>                                        
-                <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-                <NavLink icon={RiDashboardLine} href="/dashboard">Outros Relatórios</NavLink>
-                {/* <NavLink icon={RiContactsLine} href="/lancamentos">Usuários</NavLink>                     */}
-            </NavSection>
             
-            <NavSection title='ACOMPANHAMENTOS'>
-                <NavLink icon={RiInputMethodLine} href="/acompanhamentos/creditcard">Cartão de Créditos</NavLink>
-                <NavLink icon={RiGitMergeLine} href="/automation">Contas a Receber</NavLink>   
-                <NavLink icon={RiGitMergeLine} href="/acompanhamentos/billstopay">Contas a Pagar</NavLink>                  
-            </NavSection>
             
         </Stack>
     );

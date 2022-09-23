8fa7da5dea98ea0c190c5fccc884669e9bab7af8
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-09-23 07:50:00 -0400

    Implementando Icones ao menu

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -1,5 +1,5 @@
 import { Stack, Box, useBreakpointValue } from '@chakra-ui/react'
-import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from 'react-icons/ri'
+import { RiDashboardLine, RiFileList3Line, RiEdit2Line, RiAddCircleLine, RiBankCardLine, RiLogoutBoxRLine, RiNumbersLine, RiLogoutBoxLine } from 'react-icons/ri'
 import { NavSection } from './NavSection'
 import { NavLink } from './NavLink'
 
@@ -9,23 +9,26 @@ export function SidebarNav(){
         <Stack spacing="12" align="flex-start">
 
             <NavSection title='LANÇAMENTOS'>
-                <NavLink icon={RiContactsLine} href="/lancamentos/newtransaction">Nova Transação</NavLink>
-                <NavLink icon={RiContactsLine} href="/lancamentos/cadastros">Cadastros</NavLink>  
-                <NavLink icon={RiContactsLine} href="/lancamentos">Lançamentos</NavLink>                                              
+                <NavLink icon={RiAddCircleLine} href="/lancamentos/newtransaction">Nova Transação</NavLink>
+                <NavLink icon={RiFileList3Line} href="/lancamentos/cadastros">Cadastros</NavLink>  
+                <NavLink icon={RiEdit2Line} href="/lancamentos">Lançamentos</NavLink>                                              
             </NavSection>
 
             <NavSection title='RELATÓRIOS'>                                        
                 <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-                <NavLink icon={RiDashboardLine} href="/dashboard">Outros Relatórios</NavLink>
+                <NavLink icon={RiNumbersLine} href="/dashboard">Outros Relatórios</NavLink>
                 {/* <NavLink icon={RiContactsLine} href="/lancamentos">Usuários</NavLink>                     */}
             </NavSection>
             
             <NavSection title='ACOMPANHAMENTOS'>
-                <NavLink icon={RiInputMethodLine} href="/acompanhamentos/creditcard">Cartão de Créditos</NavLink>
-                <NavLink icon={RiGitMergeLine} href="/automation">Contas a Receber</NavLink>   
-                <NavLink icon={RiGitMergeLine} href="/acompanhamentos/billstopay">Contas a Pagar</NavLink>                  
+                <NavLink icon={RiBankCardLine} href="/acompanhamentos/creditcard">Cartão de Créditos</NavLink>
+                <NavLink icon={RiLogoutBoxLine} href="/automation">Contas a Receber</NavLink>   
+                <NavLink icon={RiLogoutBoxRLine} href="/acompanhamentos/billstopay">Contas a Pagar</NavLink>                  
             </NavSection>
             
         </Stack>
     );
 }
+
+
+

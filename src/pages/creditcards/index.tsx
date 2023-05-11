941f7f645f518e4ffdd075944d2a15d641cb1e85
Prisco Cleyton <you@example.comcarloshenriquepvh@hotmail.com>
2023-05-11 11:56:25 -0400

    Construindo e melhorando o componente de pesquisa

--- src/pages/creditcards/index.tsx
+++ src/pages/creditcards/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading, Icon, Divider, VStack, SimpleGrid, HStack, Button} from "@chakra-ui/react";
 import { Header } from "../../components/Header/Index";
 import { SideBar } from "../../components/Sidebar/index";
+import { NewSearchBar } from "../../components/NewSearchBar/index";
 import { Input } from "../../components/Form/Input";
 import {RiAddLine, RiPencilLine, RiSearchLine, RiFilter2Line } from 'react-icons/ri'
 import Link from 'next/link'
@@ -17,7 +18,9 @@ export default function CreditCards(){
                     <Heading size="lg" fontWeight="normal">Cartões Atuais</Heading>
                     <Divider my="6" borderColor="gray.700"></Divider>
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing="8" width="100%">                           
+
+                        <NewSearchBar/>
+                        {/* <SimpleGrid minChildWidth="240px" spacing="8" width="100%">                           
                             
                             <Button 
                                 colorScheme="orange" 
@@ -27,9 +30,6 @@ export default function CreditCards(){
                                 alignItems="center"/>}
                             >
                                 Banco Inter
-                                {/* Limite do cartão
-                                Valor consumido
-                                Próxima Fatura */}
                             </Button>
                             
                             <Link href="/lancamentos/registration/despesas" passHref>
@@ -44,7 +44,7 @@ export default function CreditCards(){
                             <Link href="/lancamentos/registration/outros" passHref>
                                 <Button colorScheme="red" h="60"fontSize="35"leftIcon={<Icon as={RiSearchLine}fontSize="35"/>}>Bradesco</Button>
                             </Link>
-                        </SimpleGrid>
+                        </SimpleGrid> */}
                         
                     </VStack>                    
 
